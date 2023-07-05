36ef4b547b5f10ea6e33dd05a294d6c079d55045
Saurabh Tandon <109807291+Tandon07@users.noreply.github.com>
2023-07-05 13:49:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'signLanguages',
     version= '0.0.0',
-    author= 'iNeuron',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'saurabh',
+    author_email= 'tandonsaurabh07@gmail.com',
     packages= find_packages(),
     install_requires = []
 
